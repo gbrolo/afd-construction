9f8aa6c4befc36dd6917926b07b22c6c0a6ae8ed
Gabriel Brolo <gabrolo13@gmail.com>
2017-07-29 11:24:20 -0600

    Dfa simulation done

--- src/Implementation/DFA.java
+++ src/Implementation/DFA.java
@@ -34,13 +34,36 @@ public class DFA {
         setInitialAndFinalStates();
     }
 
-    public boolean belongsToLanguage(String input) {
+    public String extendedDelta(String input) {
+        long start = System.nanoTime();
+        int currentState = initialStates.get(0);
         for (int i = 0; i < input.length(); i++) {
-            String currChar = Character.toString(input.charAt(i));
-            List<Transition> subTransitions = new LinkedList<>();
-
+            String currChar = Character.toString(input.charAt(i)); // current char in sequence
+            for (int j = 0; j < transitionsList.size(); j++) {
+                if (transitionsList.get(j).getInitialState().getStateId() == currentState) {
+                    if (transitionsList.get(j).getTransitionSymbol().equals(currChar)) {
+                        currentState = transitionsList.get(j).getFinalState().getStateId();
+                        j = transitionsList.size();
+                    } else if ((transitionsList.get(j).getInitialState().getNextStates().size() == 0)) {
+                        currentState = -1;
+                    }
+                }
+            }
         }
-        return true;
+        long finish = System.nanoTime();
+        long duration = (finish - start);
+
+        // traverse the list of final states to see if currentState is a final state
+        String isInLanguage = "";
+        if(finalStates.contains(currentState)) {
+            isInLanguage = " the string belongs to the language.";
+        } else {
+            isInLanguage = " the string does not belong to the language.";
+        }
+
+        String output = "The input string's final state in the DFA is " + currentState + ", therefore" + isInLanguage + "\n" +
+                "DFA search took " + duration + "ns.";
+        return output;
     }
 
     private void setTransitionsList () {
